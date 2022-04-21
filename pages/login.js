eda4f757e7074a967e09c5e4e3347c16b64ec636
Bas Mutsaers <bas@mutsaers.nu>
2022-04-21 22:19:34 +0200

    changed call

--- pages/login.js
+++ pages/login.js
@@ -55,7 +55,7 @@ export default function Login(req, res) {
                     'Content-Type': 'application/json',
                     Accept: 'application/json',
                 },
-                credentials: 'include',
+                credentials: 'same-origin',
                 body: JSON.stringify(body)
             }).then((response) => {
                 
