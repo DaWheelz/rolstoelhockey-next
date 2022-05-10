a3322e51c6d7a2f74173e4b1a5c2ba0af8064616
Bas Mutsaers <bas@mutsaers.nu>
2022-05-10 19:20:12 +0200

    deleted target _blank due to security issue

--- pages/components/footer.js
+++ pages/components/footer.js
@@ -9,10 +9,10 @@ export default function Header () {
     return (
       <footer style={{gap: '1.25rem', alignItems: 'center', flexDirection: 'column', display: 'flex', backgroundColor: '#f7f7f7', boxShadow: 'rgb(39 39 39 / 10%) 0px 1px 15px 1px', border: '1px solid rgb(218, 218, 218)'}}>
         <div style={{justifyContent: 'center', display: 'flex'}}>
-          <a style={{padding:20}} href="https://www.facebook.com/rolstoelhockey" target='_blank'>Facebook</a>
-          <a style={{padding:20}} href="https://apps.apple.com/nl/app/rolstoelhockey/id1512920070?fbclid=IwAR0r_IPRFdGomX1k70Ol1bLXhbTHPOt2by2XGc6fiTBr9f5D4jvCYBjnFPg" target='_blank'>iOS</a>
-          <a style={{padding:20}} href="https://play.google.com/store/apps/details?id=rolstoel.hockey" target='_blank'>Android</a>
-          <a style={{padding:20}} href="mailto:rolstoelhockeyapp@gmail.com" target="_blank">Contact</a>
+          <a style={{padding:20}} href="https://www.facebook.com/rolstoelhockey">Facebook</a>
+          <a style={{padding:20}} href="https://apps.apple.com/nl/app/rolstoelhockey/id1512920070?fbclid=IwAR0r_IPRFdGomX1k70Ol1bLXhbTHPOt2by2XGc6fiTBr9f5D4jvCYBjnFPg">iOS</a>
+          <a style={{padding:20}} href="https://play.google.com/store/apps/details?id=rolstoel.hockey">Android</a>
+          <a style={{padding:20}} href="mailto:rolstoelhockeyapp@gmail.com">Contact</a>
           <a style={{padding:20}} href="/policy">Privacybeleid</a>
         </div>
         <p >© 2022 Rolstoelhockey. All rights reserved.</p>
