797f1b150218075f70085a354346dfcfccf0884b
Bas Mutsaers <bas@mutsaers.nu>
2022-05-11 14:51:10 +0200

    fixed issue

--- pages/contact.js
+++ pages/contact.js
@@ -73,33 +73,33 @@ class Contact extends Component {
         return (
             <div>
       <div
-        class="max-w-screen-xl mt-20 mb-20 px-8 grid gap-8 grid-cols-1 md:grid-cols-2 md:px-12 lg:px-16 xl:px-32 mx-auto text-gray-900">
-        <div class="flex flex-col justify-between">
+        className="max-w-screen-xl mt-20 mb-20 px-8 grid gap-8 grid-cols-1 md:grid-cols-2 md:px-12 lg:px-16 xl:px-32 mx-auto text-gray-900">
+        <div className="flex flex-col justify-between">
           <div>
-            <h2 class="text-4xl lg:text-5xl font-bold leading-tight">Feedback? <br/>Ik hoor het graag!</h2>
-            <div class="text-gray-700 mt-8">
-              Liever geen formulier? Stuur een <a href="mailto:rolstoelhockeyapp@gmail.com"><span class="underline">email</span></a>
+            <h2 className="text-4xl lg:text-5xl font-bold leading-tight">Feedback? <br/>Ik hoor het graag!</h2>
+            <div className="text-gray-700 mt-8">
+              Liever geen formulier? Stuur een <a href="mailto:rolstoelhockeyapp@gmail.com"><span className="underline">email</span></a>
             </div>
           </div>
         </div>
-        <div class="">
+        <div className="">
           <div>
-            <span class="uppercase text-sm text-gray-600 font-bold">Naam</span>
-            <input class="w-full bg-gray-300 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+            <span className="uppercase text-sm text-gray-600 font-bold">Naam</span>
+            <input className="w-full bg-gray-300 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
               type="text" placeholder="" />
           </div>
-          <div class="mt-8">
-            <span class="uppercase text-sm text-gray-600 font-bold">Email</span>
-            <input class="w-full bg-gray-300 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline" type="text" />
+          <div className="mt-8">
+            <span className="uppercase text-sm text-gray-600 font-bold">Email</span>
+            <input className="w-full bg-gray-300 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline" type="text" />
           </div>
-          <div class="mt-8">
-            <span class="uppercase text-sm text-gray-600 font-bold">Bericht</span>
+          <div className="mt-8">
+            <span className="uppercase text-sm text-gray-600 font-bold">Bericht</span>
             <textarea
-              class="w-full h-32 bg-gray-300 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"></textarea>
+              className="w-full h-32 bg-gray-300 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"></textarea>
           </div>
-          <div class="mt-8">
+          <div className="mt-8">
             <button
-              class="uppercase text-sm font-bold tracking-wide bg-orange-500 text-gray-100 p-3 rounded-lg w-full focus:outline-none focus:shadow-outline">
+              className="uppercase text-sm font-bold tracking-wide bg-orange-500 text-gray-100 p-3 rounded-lg w-full focus:outline-none focus:shadow-outline">
               Verzenden
             </button>
           </div>
