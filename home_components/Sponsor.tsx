8f5b114458f1a516915fbda90b82dd20065d2eb4
Bas Mutsaers <bas@mutsaers.nu>
2022-06-09 12:02:49 +0200

    changed img object

--- home_components/Sponsor.tsx
+++ home_components/Sponsor.tsx
@@ -14,7 +14,7 @@ const Sponsors = (props: ISponsorProps) => {
     <div>
       <div className="mb-6 text-center sm:px-6">
         <div className="mt-6 mb-6 text-xl">Mede mogelijk gemaakt door : </div>
-       <Image className="rounded-xl shadow-lg mb-6" width={118} height={110} quality={100} src={`${props.image}`} />
+        <img className="rounded-xl shadow-lg mb-6" width={118} height={110} src={`${props.image}`} />
       </div>
     </div>
   );
